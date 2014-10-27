b1c872f14baeb605f43a06c6d994f379cae9c098
rspacjer <rafal.spacjer@yumasoft.pl>
2014-10-27 09:39:06 +0100

    remove everything

--- elpa/queue-0.1.1/queue-autoloads.el
+++
@@ -1,25 +0,0 @@
-;;; queue-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil "queue" "queue.el" (21559 29394 0 0))
-;;; Generated autoloads from queue.el
-
-(defalias 'make-queue 'queue-create "\
-Create an empty queue data structure.")
-
-;;;***
-
-;;;### (autoloads nil nil ("queue-pkg.el") (21559 29394 592000 0))
-
-;;;***
-
-(provide 'queue-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; queue-autoloads.el ends here
--- settings/markdown.el
+++
@@ -1,6 +0,0 @@
-(require 'markdown-mode)
-
-;;(setq-default markdown-command "pandoc -S -s --self-contained -f markdown -t html5 ")
-
-(add-to-list 'auto-mode-alist '("\\.markdown$" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
