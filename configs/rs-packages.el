20b3fd9375690f0ebce4f52b29b79b341ca1344b
rspacjer <rafal.spacjer@yumasoft.pl>
2014-10-28 08:29:36 +0100

    package installation from prelude added

--- configs/rs-packages.el
+++ configs/rs-packages.el
@@ -7,8 +7,9 @@
               '("melpa" . "http://melpa.org/packages/") t)
 (package-initialize)
 
-;; from Emacs Starter Kit - https://github.com/technomancy/emacs-starter-kit
-(defvar my-packages 
+;; from Emacs Prelude by Bozhidar Batsov
+;; https://github.com/bbatsov/prelude/blob/master/core/prelude-packages.el
+(defvar prelude-packages 
   '(powerline
     auto-complete
     auto-indent-mode
@@ -24,8 +25,34 @@
     markdown-mode
     yasnippet
     clojure-snippets
-    expand-region))
+    expand-region)
+  "A list of packages to ensure are installed at launch.")
 
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
+(defun prelude-packages-installed-p ()
+  "Check if all packages in `prelude-packages' are installed."
+  (every #'package-installed-p prelude-packages))
+
+(defun prelude-require-package (package)
+  "Install PACKAGE unless already installed."
+  (unless (memq package prelude-packages)
+    (add-to-list 'prelude-packages package))
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(defun prelude-require-packages (packages)
+  "Ensure PACKAGES are installed.
+Missing packages are installed automatically."
+  (mapc #'prelude-require-package packages))
+
+(defun prelude-install-packages ()
+  "Install all packages listed in `prelude-packages'."
+  (unless (prelude-packages-installed-p)
+    ;; check for new packages (package versions)
+    (message "%s" "Emacs is now refreshing its package database...")
+    (package-refresh-contents)
+    (message "%s" " done.")
+    ;; install the missing packages
+    (prelude-require-packages prelude-packages)))
+
+;; run package installation
+(prelude-install-packages)
