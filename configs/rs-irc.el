ce6f1b2660781849569118c0232764b92a0cf1af
Rafal Spacjer <rspacjer@gmail.com>
2015-04-08 22:36:48 +0200

    irc settings fixed

--- configs/rs-irc.el
+++ configs/rs-irc.el
@@ -1,11 +1,13 @@
 (require 'erc)
 
-(when (file-readable-p "~/.emacs.d/.erc-auth")
-  (load "~/.emacs.d/.erc-auth"))
+(when (file-readable-p "~/.emacs.d/.erc-auth.el")
+  (message "Loading data for irc username and password")
+  (load "~/.emacs.d/.erc-auth.el"))
 
 ;; Interpret mIRC-style color commands in IRC chats
 (setq erc-interpret-mirc-color t)
 
-;; (setq erc-autojoin-channels-alist '(("freenode.net" "#clojure")))
+(setq erc-autojoin-channels-alist
+      '(("freenode.net" "#clojure" "#clojure-beginners" "#emacs")))
 
 (provide 'rs-irc)
