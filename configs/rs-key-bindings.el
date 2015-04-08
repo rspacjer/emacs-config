26fc768e3a7262b615c9af485f84e8dca4a191f2
rspacjer <rafal.spacjer@yumasoft.pl>
2015-04-08 08:16:30 +0200

    key binding added

--- configs/rs-key-bindings.el
+++ configs/rs-key-bindings.el
@@ -1,1 +1,10 @@
 (provide 'rs-key-bindings)
+
+;; it's hard to reach M-x on standard keyboard, so there are two alternatives:
+;; C-x C-m
+;; C-c C-m - if you miss the x and press c
+;; based on Steve Yegge article: https://sites.google.com/site/steveyegge2/effective-emacs
+(global-set-key "\C-x\C-m" 'execute-extended-command)
+(global-set-key "\C-c\C-m" 'execute-extended-command)
+;; lets try f5 as M-x replacement
+(global-set-key (kbd "<f5>") 'execute-extended-command)
--- configs/rs-org.el
+++ configs/rs-org.el
@@ -1,3 +1,8 @@
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+
 (setq org-startup-indented t)
 (setq org-completion-use-ido t)
 
