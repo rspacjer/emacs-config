cac631ad2dd935cf91f40f76213308b3c9ab5fff
rspacjer <rafal.spacjer@yumasoft.pl>
2015-04-13 10:00:41 +0200

    electric-pair-mode disabled for org-mode

--- configs/rs-org.el
+++ configs/rs-org.el
@@ -5,9 +5,13 @@
 
 (setq org-startup-indented t)
 (setq org-completion-use-ido t)
-
 ;; add timestamp to DONE items
 (setq org-log-done 'time)
+
+;; automatic closing brackets can be painful in Org-mode
+;; i.e. inserting links is hard, because org mode will automatic display link as text
+(add-hook 'org-mode-hook
+	  (lambda () (electric-pair-mode -1)))
 
 (require 'f)
 (defun get-all-org-files ()
