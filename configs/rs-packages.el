27fd0a4894b27f53b697db6736e565daab1bb88b
rspacjer <rafal.spacjer@yumasoft.pl>
2015-04-20 09:32:41 +0200

    comment added

--- configs/rs-packages.el
+++ configs/rs-packages.el
@@ -41,6 +41,8 @@
     neotree)
   "A list of packages that should be installed from stable-melpa repository.")
 
+;; The below code is inspired by similar code from Emacs Prelude by Bozhidar Batsov
+;; https://github.com/bbatsov/prelude/blob/master/core/prelude-packages.el
 (defun rs-all-packages-installed ()
   (and (every #'package-installed-p rs-packages)
        (every #'package-installed-p rs-stable-packages)))
@@ -56,9 +58,6 @@
 (defun rs-install-packages (packages)
   (mapc #'rs-install-package-when-required packages))
 
-(rs-pin-packages "melpa" rs-packages)
-(rs-pin-packages "stable-melpa" rs-stable-packages)
-
 (defun rs-install-all-packages ()
   "Install all packages from `rs-packages' and `rs-stable-packages' lists."
   (unless (rs-all-packages-installed)
@@ -69,6 +68,9 @@
     ;; install the missing packages
     (rs-install-packages rs-packages)
     (rs-install-packages rs-stable-packages)))
+
+(rs-pin-packages "melpa" rs-packages)
+(rs-pin-packages "stable-melpa" rs-stable-packages)
 
 ;; run package installation
 (rs-install-all-packages)
