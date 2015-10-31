2617014e05609c5d990eeda5d5e843ac3c0dbb9c
Rafal Spacjer <rspacjer@gmail.com>
2015-10-31 11:04:02 +0100

    Problem with git push/pull on Windows solved

--- configs/rs-git.el
+++ configs/rs-git.el
@@ -1,7 +1,12 @@
 (require 'magit)
 
 (setq magit-last-seen-setup-instructions "1.4.0")
-
 (global-set-key (kbd "C-x g") 'magit-status)
+
+;; https://github.com/magit/magit/wiki/Pushing-with-Magit-from-Windows
+(defun rs/set-ssh-for-magit ()
+  (setenv "SSH_ASKPASS" "git-gui--askpass"))
+
+(add-hook 'rs/windows-os-hook 'rs/set-ssh-for-magit)
 
 (provide 'rs-git)
