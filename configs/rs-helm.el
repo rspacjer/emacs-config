34fa5b99de595921dca92b9e205ba7e02ae1d397
rspacjer <rafal.spacjer@yumasoft.pl>
2016-02-11 13:46:53 +0100

    Helm for recent files added

--- configs/rs-helm.el
+++ configs/rs-helm.el
@@ -4,5 +4,6 @@
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-r") 'helm-recentf)
 
 (provide 'rs-helm)
